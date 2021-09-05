62076b60de4c00bc872539ddea31b49263e3c0fb
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-09-05 22:18:29 +0200

    finish lab06

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react'
-import axios from 'axios'
+import axios from 'axios';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Img from 'react-bootstrap/Image'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 class App extends React.Component{
@@ -7,12 +12,17 @@ class App extends React.Component{
 constructor(props){
   super(props)
 
+  
+
   this.state={
     lat :'',
 
     long : '',
 
     placeName : '',
+    
+    errMassege:false,
+    mapImg:false,
 
   }
 }
@@ -24,6 +34,7 @@ const placeName=event.target.placeName.value
 const key= 'pk.4e47bf6d397576710acc509953c29e71'
 const url=`https://api.locationiq.com/v1/autocomplete.php?key=${key}&q=${placeName}`
 
+try{
 let collectedData= await axios.get(url);
 console.log(collectedData.data);
 
@@ -32,33 +43,62 @@ this.setState({
   lat:collectedData.data[0].lat,
   long:collectedData.data[0].lon,
   placeName:collectedData.data[0].display_name,
+  mapImg:true,
+
+  errMassege:false,
 
 })
 
 }
 
+
+catch
+{
+  console.log('err');
+  this.setState({
+    errMassege: true
+  })
+}
+}
+
 render(){
   return(
 
+   
+
+
+
+
+
     <>
-
-    <form onSubmit={this.getLocationinfo}>
-      
-      <input type='text' name="placeName"  />
-      <button type='submit' > Get Location</button>
-    </form>
-
-
-
-    <p>Display name : {this.state.placeName}</p>
-      <p>Lat : {this.state.lat}</p>
-      <p>Lon : {this.state.long}</p>
-
-
-    </>
-  )
-}
-
-}
-
-export default App
+    
+    <h1 style={{ padding: 25 }}>Get Your Location Now</h1>
+    <div style={{textAlign:'center'}}>
+       <Form onSubmit={this.getLocationinfo}>
+         
+         <input type='text' name="placeName"  />
+         <Button style={{ marginLeft: 60}} style={{backgroundColor: '#B61919' }} type='submit' > Get Location</Button>
+       </Form>
+       </div>
+   
+   
+   <div style={{textAlign:'center'}} >
+       <p>Display name : {this.state.placeName}</p>
+         <p>Lat : {this.state.lat}</p>
+         <p>Lon : {this.state.long}</p>
+         </div>
+   
+   
+        {  this.state.mapImg && <Img   style={{ marginLeft:'15rem' }} src={`https://maps.locationiq.com/v3/staticmap?key=pk.4e47bf6d397576710acc509953c29e71&center=${this.state.lat},${this.state.long}&size=400x400`} />}
+   
+        {this.state.errMassege && <p>You Have an Error</p>}
+         
+   
+   
+       </>
+     )
+   }
+   
+   }
+   
+   export default App
