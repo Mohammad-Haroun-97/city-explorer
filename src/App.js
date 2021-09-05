553b890c9c10a885d81c9a3bb0313529f27d8317
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-09-05 15:00:34 +0200

    finish part from lab06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+constructor(props){
+  super(props)
+
+  this.state={
+    lat :'',
+
+    long : '',
+
+    placeName : '',
+
+  }
 }
 
-export default App;
+getLocationinfo= async (event)=>{
+  event.preventDefault();
+
+const placeName=event.target.placeName.value
+const key= 'pk.4e47bf6d397576710acc509953c29e71'
+const url=`https://api.locationiq.com/v1/autocomplete.php?key=${key}&q=${placeName}`
+
+let collectedData= await axios.get(url);
+console.log(collectedData.data);
+
+
+this.setState({
+  lat:collectedData.data[0].lat,
+  long:collectedData.data[0].lon,
+  placeName:collectedData.data[0].display_name,
+
+})
+
+}
+
+render(){
+  return(
+
+    <>
+
+    <form onSubmit={this.getLocationinfo}>
+      
+      <input type='text' name="placeName"  />
+      <button type='submit' > Get Location</button>
+    </form>
+
+
+
+    <p>Display name : {this.state.placeName}</p>
+      <p>Lat : {this.state.lat}</p>
+      <p>Lon : {this.state.long}</p>
+
+
+    </>
+  )
+}
+
+}
+
+export default App
