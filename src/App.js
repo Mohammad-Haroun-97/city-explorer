a9d3e0dc29fe90478fbd02f7794cdf0553888387
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-09-10 11:11:36 +0200

    final result lab06

--- src/App.js
+++ src/App.js
@@ -1,121 +1,119 @@
-import axios from 'axios';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Table from 'react-bootstrap/Table'
-import Img from 'react-bootstrap/Image'
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import Weather from './component/weather';
+import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Table from "react-bootstrap/Table";
+import Img from "react-bootstrap/Image";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Weather from "./component/weather";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-
-
-class App extends React.Component{
-
-constructor(props){
-  super(props)
-
-  
-
-  this.state={
-    lat :'',
-    long : '',
-    placeName : '',
-    errMassege:false,
-    mapImg:false,
-    showWeather: false,
-    weatherErr: '',
-    renderedLocWeatherData:[],
-
+    this.state = {
+      lat: "",
+      long: "",
+      placeName: "",
+      errMassege: false,
+      mapImg: false,
+      // showWeather: false,
+      weatherErr: "",
+      // renderedLocWeatherData:[],
+    };
   }
-}
 
-getLocationinfo= async (event)=>{
-  event.preventDefault();
+  getLocationinfo = async (event) => {
+    event.preventDefault();
 
-const placeName=event.target.cityName.value
-const REACT_APP_KEY= 'pk.43fed3791d35ddb76aa14f749c6d3080'
-const url=`https://api.locationiq.com/v1/autocomplete.php?key=${REACT_APP_KEY}&q=${placeName}`
+    let selectCity = event.target.userInput.value;
 
-let serverRoute=process.env.REACT_APP_SERVER
-let weatherData = `https://new-cit-api-haroun.herokuapp.com/weather?searchQuery=${placeName}`;
+    // let serverRoute = process.env.REACT_APP_SERVER;
 
-try{
-let collectedData= await axios.get(url);
-console.log(collectedData.data);
+    const url = `https://eu1.locationiq.com/v1/search.php?key=pk.43fed3791d35ddb76aa14f749c6d3080&q=${selectCity}&format=json`;
 
+    try {
+      let collectedData = await axios.get(url);
+      console.log(collectedData.data);
 
-this.setState({
-  lat:collectedData.data[0].lat,
-  long:collectedData.data[0].lon,
-  placeName:collectedData.data[0].display_name,
-  mapImg:true,
+      this.setState({
+        lat: collectedData.data[0].lat,
+        long: collectedData.data[0].lon,
+        placeName: collectedData.data[0].display_name,
+        mapImg: true,
+        errMassege: false,
+      });
+    } catch {
+      console.log("err");
+      this.setState({
+        errMassege: true,
+        mapImg: false,
+      });
+    }
 
-  errMassege:false,
+    // try {
 
-})
+    //   const weatherData = `http://api.weatherbit.io/v2.0/forecast/daily?city=${selectCity}&key=e2c95883c34745f58ae63470e722f634&format=json`;
 
-}
+    //   let frontWeatherData = await axios.get(weatherData);
 
+    //     this.setState({
+    //       renderedLocWeatherData: frontWeatherData.data,
+    //       // showWeather: true,
 
-catch
-{
-  console.log('err');
-  this.setState({
-    errMassege: true
-  })
-}
+    //     })
 
+    // }
+    // catch(error) {
+    //   this.setState({
+    //     renderedLocWeatherData: error.response,
+    //     // showWeather: false,
 
-try {
-  let frontWeatherData = await axios.get(weatherData);
-  
-    this.setState({
-      renderedLocWeatherData: frontWeatherData  ,
-      showWeather: true,
-      
-    })
-  
-}
-catch {
-  this.setState({
-    weatherErr: 'sorry , no weather data availabe for your location',
-    showWeather: false,
-    
+    //   })
 
-  })
+    // }
+  };
 
-}
-}
+  render() {
+    return (
+      <>
+        <h1 style={{ padding: 25 }}>Get Your Location Now</h1>
+        <div style={{ textAlign: "center" }}>
+          <Form onSubmit={this.getLocationinfo}>
+            <input type="text" name="userInput" />
+            <Button
+              style={{ marginLeft: 60 }}
+              style={{ backgroundColor: "#B61919" }}
+              type="submit"
+            >
+              {" "}
+              Get Location
+            </Button>
+          </Form>
+        </div>
 
-render(){
-  return(
+        <div style={{ textAlign: "center" }}>
+          <p>Display name : {this.state.placeName}</p>
+          <p>Lat : {this.state.lat}</p>
+          <p>Lon : {this.state.long}</p>
+        </div>
 
-    <>
-    
-    <h1 style={{ padding: 25 }}>Get Your Location Now</h1>
-    <div style={{textAlign:'center'}}>
-       <Form onSubmit={this.getLocationinfo}>
-         
-         <input type='text' name="cityName"  />
-         <Button style={{ marginLeft: 60}} style={{backgroundColor: '#B61919' }} type='submit' > Get Location</Button>
-       </Form>
-       </div>
-   
-   
-   <div style={{textAlign:'center'}} >
-       <p>Display name : {this.state.placeName}</p>
-         <p>Lat : {this.state.lat}</p>
-         <p>Lon : {this.state.long}</p>
-         </div>
-   
-   
-        {  this.state.mapImg && <Img   style={{ marginLeft:'29rem' }} src={`https://maps.locationiq.com/v3/staticmap?key=pk.43fed3791d35ddb76aa14f749c6d3080&center=${this.state.lat},${this.state.long}&size=400x400`} />}
-   
-        {/* {this.state.errMassege && <p>You Have an Error</p>} */}
+        {this.state.mapImg && (
+          <Img
+            style={{ marginLeft: "29rem" }}
+            src={`https://maps.locationiq.com/v3/staticmap?key=pk.43fed3791d35ddb76aa14f749c6d3080&center=${this.state.lat},${this.state.long}&size=400x400`}
+          />
+        )}
 
-        <div>
-            {
+        {this.state.errMassege && (
+          <Card>
+            <Card.Body>Error, The city Center is not valid</Card.Body>
+          </Card>
+        )}
+
+        {/* <div> */}
+        {/* {
               this.state.renderedLocWeatherData.map((weather) => {
                 return (
                   <div>
@@ -123,15 +121,11 @@ render(){
                     <p>{weather.description} </p>
                   </div>
                 );
-              })}
-          </div>
+              })} */}
+        {/* </div> */}
+      </>
+    );
+  }
+}
 
-          
-
-       </>
-     )
-   }
-   
-   }
-   
-   export default App
+export default App;
